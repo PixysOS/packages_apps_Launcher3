omnirom/android_packages_apps_Launcher3

Launcher3: fix preview rendering after adding qsb switch
Change-Id: I4f4dfe33f6c71de68c3216e0a26f5a71fcd6c762

src/com/android/launcher3/qsb/QsbContainerView.java
@@ -215,7 +215,10 @@ public class QsbContainerView extends FrameLayout {
         }
 
         public boolean isQsbEnabled() {
-            return Utilities.showQsbWidget(getActivity());
+            if (!isInPreviewMode()) {
+                return Utilities.showQsbWidget(getActivity());
+            }
+            return true;
         }
 
         protected Bundle createBindOptions() {

PixysOS/packages_apps_Launcher3

Signed-off-by: DennySPB <dennyspb@gmail.com>
Signed-off-by: DennySPb <dennyspb@gmail.com>

src/com/android/launcher3/qsb/QsbContainerView.java
@@ -287,7 +287,10 @@ public class QsbContainerView extends FrameLayout {
         }
 
         public boolean isQsbEnabled() {
-            return Utilities.showQSB(getContext());
+            if (!isInPreviewMode()) {
+                return Utilities.showQSB(getContext());
+            }
+            return true;
         }
 
         protected Bundle createBindOptions() {